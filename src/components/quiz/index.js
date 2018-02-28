807f0f1e6904321df51904c538270b95c5514d44
Sahu, S <sauravsahu.12@live.com>
2018-02-28 22:57:08 +0530

    Add styling for quiz component.
    Add styling for quiz component.

--- src/components/quiz/index.js
+++ src/components/quiz/index.js
@@ -3,20 +3,24 @@ import React from 'react';
 import Question from '../question';
 import TitleBar from '../title-bar';
 
+import * as styles from './quiz.style';
+
 const Quiz = props => (
   <div>
     <TitleBar
       brand="Quizzy"
       message={`Hello ${props.userName}`}
     />
-    {props.questions.map((question, index) => (
-      <Question
-        question={question}
-        number={index + 1}
-        onOptionClick={(questionId, selectedAnswer) =>
-          props.onOptionClick(questionId, selectedAnswer)}
-      />
-    ))}
+    <div style={styles.questions}>
+      {props.questions.map((question, index) => (
+        <Question
+          question={question}
+          number={index + 1}
+          onOptionClick={(questionId, selectedAnswer) =>
+            props.onOptionClick(questionId, selectedAnswer)}
+        />
+      ))}
+    </div>
     <button
       disabled={!props.calculateEnabled}
       onClick={() => props.onCalculateButtonClick()}
---
+++ src/components/quiz/quiz.style.js
@@ -0,0 +1,7 @@
+export const questions = {
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '0 32px',
+};
+
+export default questions;
